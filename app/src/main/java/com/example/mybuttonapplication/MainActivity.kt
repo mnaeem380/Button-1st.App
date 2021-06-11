6081dbaa69f13117cd2885c726ae5d81f284c648
zahira <zahira-arain786@outlook.com>
2021-06-11 10:36:40 +0500

    code sent 0n 11.06.21

---
+++ app/src/main/java/com/example/mybuttonapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mybuttonapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    btnShowToast.setOnClickListener {
+        Log.i("MainActivity", "Button Was Clicked !")
+        Toast.makeText(this, "Button Was Clicked !", Toast.LENGTH_SHORT ).show()
+
+    }
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+            val intent= Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
